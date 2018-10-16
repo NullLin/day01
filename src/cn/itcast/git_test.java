205732926137349d6dafdb7e3f14df66a71a3300
chenlin <361919017@qq.com>
2018-10-16 22:00:41 +0800

    将工程添加至git

---
+++ src/cn/itcast/git_test.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hhhhhhh");
+        System.out.println("hhhhhhh");
+        System.out.println("hhhhhhh");
+        System.out.println("hhhhhhh");
+    }
+}
